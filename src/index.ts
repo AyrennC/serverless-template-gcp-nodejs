a4f706bb622c3bf3c023340419a04c529d871402
Carney Cheng <carney.cheng@webssup.com>
2020-01-15 13:15:36 +0800

    updated service name, and changed handler name

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 import createHelloHandler from './Handlers/createHelloHandler';
 
 // eslint-disable-next-line @typescript-eslint/camelcase
-exports.create_folder = createHelloHandler();
+exports.hello_word = createHelloHandler();
